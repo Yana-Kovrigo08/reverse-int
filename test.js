3f8630e2c38b1866fae0858125488452c3557de4
Yana-Kovrigo08 <106354510+Yana-Kovrigo08@users.noreply.github.com>
2023-01-28 17:27:15 +0000

    feat: reverse

--- test.js
+++ test.js
@@ -1,104 +1,74 @@
 const assert = require('assert');
 
-const reverse = require('./src');
+const towelSort = require('./src/index.js');
 
-it('Should return 261 when 162 given', () => {
-  const reversed = reverse(162);
+describe('.towelSort, should perfrom \'towelSort\'', () => {
 
-  assert.equal(reversed, 261);
-});
-it('Should return 291 when -192 given', () => {
-  const reversed = reverse(-192);
+  it('Should return empty array if no params passed', () => {
+    assert.deepEqual(towelSort(), []);
+  });
 
-  assert.equal(reversed, 291);
-});
-it('Should return 252 when -252 given', () => {
-  const reversed = reverse(-252);
+  it('Should return empty array if matrix is empty', () => {
+    assert.deepEqual(towelSort([]), []);
+  });
 
-  assert.equal(reversed, 252);
-});
-it('Should return 71 when 170 given', () => {
-  const reversed = reverse(170);
+  it('should do towelSort', () => {
+    const matrix = [
+      [1, 2],
+      [3, 4],
+    ];
 
-  assert.equal(reversed, 71);
-});
-it('Should return 605 when 506 given', () => {
-  const reversed = reverse(506);
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 3 ]);
+  });
 
-  assert.equal(reversed, 605);
-});
-it('Should return 835 when -538 given', () => {
-  const reversed = reverse(-538);
+  it('should do towelSort', () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
 
-  assert.equal(reversed, 835);
-});
-it('Should return 465 when 564 given', () => {
-  const reversed = reverse(564);
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, 7, 8, 9 ]);
+  });
 
-  assert.equal(reversed, 465);
-});
-it('Should return 891 when -198 given', () => {
-  const reversed = reverse(-198);
+  it('should do towelSort', () => {
+    const matrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+    ];
 
-  assert.equal(reversed, 891);
-});
-it('Should return 715 when -517 given', () => {
-  const reversed = reverse(-517);
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 16, 15, 14, 13 ]);
+  });
 
-  assert.equal(reversed, 715);
-});
-it('Should return 601 when -106 given', () => {
-  const reversed = reverse(-106);
+  it('should do towelSort correctrly with nonsquare matrix', () => {
+    const matrix = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+    ];
 
-  assert.equal(reversed, 601);
-});
-it('Should return 462 when -264 given', () => {
-  const reversed = reverse(-264);
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12,]);
+  });
 
-  assert.equal(reversed, 462);
-});
-it('Should return 332 when 233 given', () => {
-  const reversed = reverse(233);
+  it('should do towelSort', () => {
+    const matrix = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ];
 
-  assert.equal(reversed, 332);
-});
-it('Should return 763 when -367 given', () => {
-  const reversed = reverse(-367);
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 3, 6, 5, 4, ]);
+  });
 
-  assert.equal(reversed, 763);
-});
-it('Should return 25 when 520 given', () => {
-  const reversed = reverse(520);
+  it('should do towelSort', () => {
+    const matrix = [
+        [1, 2, 4],
+        [5, 6, 7, 8],
+        [9, 12],
+    ];
 
-  assert.equal(reversed, 25);
-});
-it('Should return 822 when 228 given', () => {
-  const reversed = reverse(228);
+    assert.deepEqual(towelSort(matrix), [ 1, 2, 4, 8, 7, 6, 5, 9, 12 ]);
+  });
 
-  assert.equal(reversed, 822);
-});
-it('Should return 231 when -132 given', () => {
-  const reversed = reverse(-132);
-
-  assert.equal(reversed, 231);
-});
-it('Should return 152 when -251 given', () => {
-  const reversed = reverse(-251);
-
-  assert.equal(reversed, 152);
-});
-it('Should return 561 when 165 given', () => {
-  const reversed = reverse(165);
-
-  assert.equal(reversed, 561);
-});
-it('Should return 524 when -425 given', () => {
-  const reversed = reverse(-425);
-
-  assert.equal(reversed, 524);
-});
-it('Should return 535 when 535 given', () => {
-  const reversed = reverse(535);
-
-  assert.equal(reversed, 535);
 });
